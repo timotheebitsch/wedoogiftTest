aed1682075778a27dcc7da09bfd5bedaeb78c28f
Tim <Tim@PC-Tim.home>
2024-01-14 16:05:05 +0100

    Correction of TU POST

--- backend/Wedoogift2/src/test/java/com/example/demo/controller/DistributionControllerTest.java
+++ backend/Wedoogift2/src/test/java/com/example/demo/controller/DistributionControllerTest.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -12,13 +14,17 @@ import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.example.demo.controller.dto.CompanyDistribution;
+import com.example.demo.controller.dto.ItemType;
 import com.example.demo.service.BalanceCalculatorService;
 import com.example.demo.service.DistributionService;
-
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(DistributionController.class)
@@ -33,12 +39,11 @@ public class DistributionControllerTest {
     @MockBean
     private BalanceCalculatorService balanceCalculatorService;
     @Captor
-    private ArgumentCaptor<Long> idCaptor;
-    
+    private ArgumentCaptor<Long> idCaptor;    
 
     @Test
     public void printCompanyBalanceTest() throws Exception {
-        this.mvc.perform(get("http://localhost:8088/print-company-balance/1"))
+        this.mvc.perform(MockMvcRequestBuilders.get("/print-company-balance/1"))
                 .andExpect(status().isOk());
         
         verify(balanceCalculatorService).printCompanyBalance(idCaptor.capture());
@@ -50,7 +55,7 @@ public class DistributionControllerTest {
     
     @Test
     public void printUserBalanceTest() throws Exception {
-        this.mvc.perform(get("http://localhost:8088/print-user-balance/1"))
+        this.mvc.perform(MockMvcRequestBuilders.get("/print-user-balance/1"))
                 .andExpect(status().isOk());
         
         verify(balanceCalculatorService).printUserBalance(idCaptor.capture());
@@ -59,11 +64,32 @@ public class DistributionControllerTest {
         Long capturedId = idCaptor.getValue();
         assertTrue(capturedId.equals(1L));
     }
-    
+      
     @Test
     public void printAllBalanceTest() throws Exception {
-        this.mvc.perform(get("http://localhost:8088/print-all-balance"))
+        this.mvc.perform(MockMvcRequestBuilders.get("/print-all-balance"))
                 .andExpect(status().isOk());
+    }
+    
+    @Test
+    public void depositItemTest() throws Exception {
+        CompanyDistribution companyDistribution = new CompanyDistribution(1L, 1L, ItemType.GIFT, LocalDate.of(2024, 05, 01) , 60);
+        when(distributionService.distribute(companyDistribution)).thenReturn(true);
+
+        this.mvc.perform(MockMvcRequestBuilders.post("/deposit")
+        .content(asJsonString(companyDistribution))
+		.contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+    }
+    
+    public static String asJsonString(final Object obj) {
+    	ObjectMapper objectMapper = new ObjectMapper();
+    	objectMapper.registerModule(new JavaTimeModule());
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
